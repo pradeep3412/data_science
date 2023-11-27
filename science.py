53bb3776bb23d728cfa12d619dac80f5b5bfc742
pradeep3412 <139706114+pradeep3412@users.noreply.github.com>
2023-11-27 21:02:49 +0530

    Create science.py

---
+++ science.py
@@ -0,0 +1,46 @@
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the Iris dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+iris_df = pd.read_csv(url, names=column_names)
+
+# Explore the dataset
+print(iris_df.head())
+
+# Visualize the data
+sns.pairplot(iris_df, hue='class', diag_kind='kde')
+plt.show()
+
+# Prepare the data for training
+X = iris_df.drop('class', axis=1)
+y = iris_df['class']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Train a k-nearest neighbors (KNN) classifier
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train_scaled, y_train)
+
+# Make predictions on the test set
+y_pred = knn.predict(X_test_scaled)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+# Display classification report
+print("Classification Report:")
+print(classification_report(y_test, y_pred))
